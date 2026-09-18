eca21fed7d63a35723f1b29d7410b5a3beb244dd
probstjano <jano.probst@orange.fr>
2026-09-18 08:22:31 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'OMNIA-App-v7.8'; // Version hochzählen bei jedem Update
+const CACHE_NAME = 'OMNIA-App-v7.9'; // Version hochzählen bei jedem Update
 
 const ASSETS_TO_CACHE = [
   'index.html',
